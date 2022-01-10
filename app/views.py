c4b06ec694d96034b2d4b34395bd50773668d27d
derrickmacharia <derrick.macharia@student.moringaschool.com>
2022-01-10 08:59:06 +0300

    create search view function

--- app/views.py
+++ app/views.py
@@ -173,3 +173,15 @@ def businesses(request):
         neighborhood = profile.neighbourhood
         businesses = Business.objects.filter(user_id=current_user.id)
         return render(request, "business/business.html", {"businesses": businesses})
+
+@login_required(login_url="/accounts/login/")
+def search_business(request):
+    if 'search_term' in request.GET and request.GET["search_term"]:
+        search_term = request.GET.get("search_term")
+        searched_businesses = Business.objects.filter(name__icontains=search_term)
+        message = f"Search For: {search_term}"
+
+        return render(request, "search.html", {"message": message, "businesses": searched_businesses})
+    else:
+        message = "You haven't searched for any term"
+        return render(request, "search.html", {"message": message})
